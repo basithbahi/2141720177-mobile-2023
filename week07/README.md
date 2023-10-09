3da0bbc596961e3edce72a3219347b02026e12bb
basithbahi <basithbahi@gmail.com>
2023-10-09 10:51:13 +0700

    WEEK 07

---
+++ week07/README.md
@@ -0,0 +1,81 @@
+# Praktikum 1: Membangun Layout di Flutter
+
+<b>Tampilan akhir yang akan Anda buat</b>
+
+<b>Langkah 1: Buat Project Baru</b>
+
+Buatlah sebuah project flutter baru dengan nama layout_flutter. Atau sesuaikan style laporan praktikum yang Anda buat.
+
+<b>Langkah 2: Buka file lib/main.dart</b>
+
+Buka file main.dart lalu ganti dengan kode berikut. Isi nama dan NIM Anda di text title.
+
+![Alt text](image.png)
+
+<b>Langkah 3: Identifikasi layout diagram</b>
+
+Langkah pertama adalah memecah tata letak menjadi elemen dasarnya:
+
+a. Identifikasi baris dan kolom.
+
+b. Apakah tata letaknya menyertakan kisi-kisi (grid)?
+
+c. Apakah ada elemen yang tumpang tindih?
+
+d. Apakah UI memerlukan tab?
+
+e. Perhatikan area yang memerlukan alignment, padding, atau borders.
+
+Pertama, identifikasi elemen yang lebih besar. Dalam contoh ini, empat elemen disusun menjadi sebuah kolom: sebuah gambar, dua baris, dan satu blok teks.
+
+![Alt text](image-1.png)
+
+Selanjutnya, buat diagram setiap baris. Baris pertama, disebut bagian Judul, memiliki 3 anak: kolom teks, ikon bintang, dan angka. Anak pertamanya, kolom, berisi 2 baris teks. Kolom pertama itu memakan banyak ruang, sehingga harus dibungkus dengan widget yang Diperluas.
+
+![Alt text](image-2.png)
+
+Baris kedua, disebut bagian Tombol, juga memiliki 3 anak: setiap anak merupakan kolom yang berisi ikon dan teks.
+
+![Alt text](image-3.png)
+
+Setelah tata letak telah dibuat diagramnya, cara termudah adalah dengan menerapkan pendekatan bottom-up. Untuk meminimalkan kebingungan visual dari kode tata letak yang banyak bertumpuk, tempatkan beberapa implementasi dalam variabel dan fungsi.
+
+<b>Langkah 4: Implementasi title row</b>
+
+Pertama, Anda akan membuat kolom bagian kiri pada judul. Tambahkan kode berikut di bagian atas metode build() di dalam kelas MyApp:
+
+![Alt text](image-4.png)
+
+<b>Soal 1</b>
+
+    Letakkan widget Column di dalam widget Expanded agar menyesuaikan ruang yang tersisa di dalam widget Row. Tambahkan properti crossAxisAlignment ke CrossAxisAlignment.start sehingga posisi kolom berada di awal baris.
+
+<b>Soal 2</b>
+
+    Letakkan baris pertama teks di dalam Container sehingga memungkinkan Anda untuk menambahkan padding = 8. Teks ‘Batu, Malang, Indonesia' di dalam Column, set warna menjadi abu-abu.
+
+<b>Soal 3</b>
+
+    Dua item terakhir di baris judul adalah ikon bintang, set dengan warna merah, dan teks "41". Seluruh baris ada di dalam Container dan beri padding di sepanjang setiap tepinya sebesar 32 piksel. Kemudian ganti isi body text ‘Hello World' dengan variabel titleSection seperti berikut:
+
+![Alt text](image-5.png)
+
+<b>Jawab:</b>
+
+![Alt text](image-6.png)
+
+# Praktikum 2: Implementasi button row
+
+<b>Jawab:</b>
+
+![Alt text](image-7.png)
+
+# Praktikum 3: Implementasi text section
+
+<b>Jawab:</b>
+
+![Alt text](image-8.png)
+
+# <B> HASIL AKHIR:
+
+![Alt text](image-9.png)
---
+++ week07/lib/main.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Widget image = Expanded(child: Image.asset('jatimpark.png'));
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(20),
+      child: Row(
+        children: [
+          // Expanded(child: Image.asset('jatimpark.png')),
+          Expanded(
+            /* soal 1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /* soal 2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 20),
+                  child: const Text(
+                    'JATIMPARK 1',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Jl. Kartika No.2, Sisir, Kec. Batu, Kota Batu, Jawa Timur 65315',
+                  style: TextStyle(
+                    color: Colors.black26,
+                    fontWeight: FontWeight.w800,
+                    fontFamily: 'Times New Roman',
+                    letterSpacing: 0.5,
+                    fontSize: 15,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          /* soal 3*/
+          Icon(
+            Icons.star,
+            color: Colors.orange[500],
+          ),
+          const Text(
+            '5',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w800,
+              fontFamily: 'Roboto',
+              letterSpacing: 0.5,
+              fontSize: 20,
+            ),
+          ),
+        ],
+      ),
+    );
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(15),
+      child: const Text(
+        'Jawa Timur Park Satu adalah Taman Hiburan dengan Tema Pameran adat budaya Indonesia yang dilengkapi dengan pembelajaran Science Center. '
+        'Abdul Basith Bahi ',
+        softWrap: true,
+      ),
+    );
+
+    return MaterialApp(
+      title: 'Abdul Basith Bahi | 2141720177',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
+        ),
+        body: Column(
+          children: [image, titleSection, buttonSection, textSection],
+        ),
+      ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
